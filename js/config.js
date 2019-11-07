a29ffb840e07578ff9d280ab189eff8a473a6181
mr-coxall <mr.coxall@mths.ca>
2019-11-07 03:36:02 +0000

    initial checkin of website code

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_teQ5TT9Ly', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '17kp1lqjkjq2lj20i1hora2u9o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://xnbxfpw731.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
